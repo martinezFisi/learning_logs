9359cb677967eb0ea995221abb01d0e4d2b11373
Antony Martinez <antonymartinez12@gmail.com>
2025-11-10 22:06:44 -0500

    Se agrega app accounts para manejar los usuarios y su autenticacion

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path,include
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+    #include default auth urls
+    path('', include('django.contrib.auth.urls')),
+]
--- ll_project/settings.py
+++ ll_project/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
     # My apps
     'learning_logs',
+    'accounts',
 
     # Default Django apps
     'django.contrib.admin',
@@ -125,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# My settings
+LOGIN_REDIRECT_URL = 'learning_logs:index'
